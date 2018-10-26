15d77c44a1eb6c0ee7ed43a661467ade4208c064
pr293504 <pr293504@wipro.com>
2018-10-26 20:55:35 +0530

    cors

--- runner/fav.js
+++ runner/fav.js
@@ -9,7 +9,13 @@ const db = require('../db.json');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
+router.use(function(req, res, next) {
+	
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 router.route('/')
